fc33234e5bdb8ab63ded101e6b3a1afc216e1a6d
ethers <ethereum@outlook.com>
2014-08-26 02:11:11 -0700

    mine(); todo struct.pack

--- src/chainManager.js
+++ src/chainManager.js
@@ -23,6 +23,38 @@ var Miner = function(opts) {
 //    logger.debug('Difficulty %s', self.block.difficulty)
 };
 
+
+var TWO_POW_256 = BigInteger('2').pow(256);
+Miner.prototype.mine = function(steps) {
+    steps = steps || 1000;
+
+    // ??
+    //var nonce_bin_prefix = '\x00' * (32 - len(struct.pack('>q', 0)))
+
+    var nonce_bin_prefix = util.repeat('\x00', 24);
+    var target = TWO_POW_256.divide(this.block.difficulty);
+    var rlp_Hn = this.block.serialize_header_without_nonce();
+
+    var stopLoop = this.nonce + steps;
+    for (var nonce = this.nonce; nonce < stopLoop; nonce++) {
+        var nonce_bin = nonce_bin_prefix + struct.pack('>q', nonce)
+        // BE(SHA3(SHA3(RLP(Hn)) o n))
+        var h = util.sha3(util.sha3(rlp_Hn) + nonce_bin);
+        var l256 = util.bigEndianToInt(h);
+        if (l256 < target) {
+            this.block.nonce = nonce_bin;
+//            assert self.block.check_proof_of_work(self.block.nonce) is True
+//            assert self.block.get_parent()
+//            logger.debug(
+//                'Nonce found %d %r', nonce, self.block)
+            return this.block;
+        }
+    }
+
+    this.nonce = nonce;
+    return false;
+}
+
 module.exports = {
     Miner: Miner
 };
